2307f0cd80864107ed79abfab1fe00195a8088e5
alejandro-du <alejandro.d.a@gmail.com>
2022-12-08 00:59:17 +0200

    Remove MaxScale

--- build.sh
+++ build.sh
@@ -3,6 +3,5 @@ echo "************************ build *************************"
 
 docker build --platform linux/arm --file primary/Dockerfile --tag alejandrodu/mariadb-arm-primary .
 docker build --platform linux/arm --file replica/Dockerfile --tag alejandrodu/mariadb-arm-replica .
-#docker build --file maxscale/Dockerfile --tag alejandrodu/mariadb-arm-maxscale .
 
 echo
--- test.sh
+++ test.sh
@@ -11,5 +11,3 @@ docker stack deploy -c stacks/replication.stack.yml mariadb
 sleep 15
 
 docker exec -it $(docker ps | grep replica.1 | grep -o '[^ ]\+$' | tail -1) mariadb --password=password -e "show replica status\G" | grep 'Slave_IO_Running\|Slave_SQL_Running\|Last_IO_Error\|Last_SQL_Error'
-
-#docker exec -it $(docker ps | grep replica.1 | grep -o '[^ ]\+$' | tail -1) mariadb --password=password -e "reset replica; start replica; select sleep(1); show replica status\G"
