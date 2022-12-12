14604841cf18b7f02e4eea5cefec66cb4c9c4832
alejandro-du <alejandro.d.a@gmail.com>
2022-12-12 20:26:21 +0200

    Use Docker Hub

--- multiplatform-build-and-publish.sh
+++ multiplatform-build-and-publish.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "************************ publish *************************"
-host=${1:-thinkpad.local:5000}
+host=$1
 
-docker buildx build --push --platform linux/amd64,linux/arm64 --file single-node/Dockerfile --tag $host/alejandrodu/mariadb-single-node .
-docker buildx build --push --platform linux/amd64,linux/arm64 --file primary/Dockerfile 	--tag $host/alejandrodu/mariadb-primary .
-docker buildx build --push --platform linux/amd64,linux/arm64 --file replica/Dockerfile 	--tag $host/alejandrodu/mariadb-replica .
-docker buildx build --push --platform linux/amd64,linux/arm64 --file maxscale/Dockerfile 	--tag $host/alejandrodu/mariadb-maxscale .
+docker buildx build --push --platform linux/amd64,linux/arm64 --file single-node/Dockerfile --tag ${host}alejandrodu/mariadb-single-node .
+docker buildx build --push --platform linux/amd64,linux/arm64 --file primary/Dockerfile 	--tag ${host}alejandrodu/mariadb-primary .
+docker buildx build --push --platform linux/amd64,linux/arm64 --file replica/Dockerfile 	--tag ${host}alejandrodu/mariadb-replica .
+docker buildx build --push --platform linux/amd64,linux/arm64 --file maxscale/Dockerfile 	--tag ${host}alejandrodu/mariadb-maxscale .
 
 echo
--- publish.sh
+++ publish.sh
@@ -1,10 +1,10 @@
 echo "************************ publish *************************"
-host=${1:-thinkpad.local:5000}
+host="$1"
 
-docker tag alejandrodu/mariadb-single-node $host/alejandrodu/mariadb-single-node
-docker tag alejandrodu/mariadb-primary $host/alejandrodu/mariadb-primary
-docker tag alejandrodu/mariadb-replica $host/alejandrodu/mariadb-replica
+docker tag alejandrodu/mariadb-single-node ${host}alejandrodu/mariadb-single-node
+docker tag alejandrodu/mariadb-primary ${host}alejandrodu/mariadb-primary
+docker tag alejandrodu/mariadb-replica ${host}alejandrodu/mariadb-replica
 
-docker push $host/alejandrodu/mariadb-single-node
-docker push $host/alejandrodu/mariadb-primary
-docker push $host/alejandrodu/mariadb-replica
+docker push ${host}alejandrodu/mariadb-single-node
+docker push ${host}alejandrodu/mariadb-primary
+docker push ${host}alejandrodu/mariadb-replica
