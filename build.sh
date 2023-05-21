02abb050b42b4171efd0bf4e7a1584456eed07e9
Alejandro Duarte <alejandro.d.a@gmail.com>
2023-05-21 15:35:36 +0300

    Fix maxscale image tag

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ if [ "$1" == "es" ]; then
     docker build --file mariadb-es/Dockerfile --build-arg CUSTOMER_DOWNLOAD_TOKEN=$CUSTOMER_DOWNLOAD_TOKEN --tag alejandrodu/mariadb-es .
 else
 	docker build --file mariadb/Dockerfile --tag alejandrodu/mariadb .
-	docker build --file maxscale/Dockerfile --tag mariadb-maxscale .
+	docker build --file maxscale/Dockerfile --tag alejandrodu/mariadb-maxscale .
 fi
 
 echo
