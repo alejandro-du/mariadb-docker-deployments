7d7d72a664e16e2a2f3ecbc257cb0823c50acdc2
Alejandro Duarte <alejandro.d.a@gmail.com>
2023-07-24 17:15:43 +0300

    Fix multi-arch build

--- multiplatform-build-and-publish.sh
+++ multiplatform-build-and-publish.sh
@@ -8,11 +8,10 @@ else
 	host=$1
 fi
 
-docker buildx build --push --platform linux/amd64 --file mariadb/Dockerfile --tag ${host}alejandrodu/mariadb .
-docker buildx build --push --platform linux/arm64 --file mariadb/Dockerfile --tag ${host}alejandrodu/mariadb .
+docker buildx use custom-builder
+docker buildx build --push --platform linux/amd64,linux/arm64 --file mariadb/Dockerfile --tag ${host}alejandrodu/mariadb .
 
-docker buildx build --push --platform linux/amd64 --file maxscale/Dockerfile --tag ${host}alejandrodu/mariadb-maxscale .
-docker buildx build --push --platform linux/arm64 --file maxscale/Dockerfile --tag ${host}alejandrodu/mariadb-maxscale .
+docker buildx build --push --platform linux/amd64,linux/arm64 --file maxscale/Dockerfile --tag ${host}alejandrodu/mariadb-maxscale .
 
 if [ "$es" ]; then
 	echo "!!! PUBLISHING MARIADB ENTERPRISE SERVER TO $host !!!"
@@ -20,8 +19,7 @@ if [ "$es" ]; then
 
 	case $yn in 
 		yes ) echo OK...
-			docker buildx build --push --build-arg CUSTOMER_DOWNLOAD_TOKEN=$CUSTOMER_DOWNLOAD_TOKEN --platform linux/amd64 --file mariadb-es/Dockerfile --tag ${host}alejandrodu/mariadb-es .
-			docker buildx build --push --build-arg CUSTOMER_DOWNLOAD_TOKEN=$CUSTOMER_DOWNLOAD_TOKEN --platform linux/arm64 --file mariadb-es/Dockerfile --tag ${host}alejandrodu/mariadb-es .
+			docker buildx build --push --build-arg CUSTOMER_DOWNLOAD_TOKEN=$CUSTOMER_DOWNLOAD_TOKEN --platform linux/amd64,linux/arm64 --file mariadb-es/Dockerfile --tag ${host}alejandrodu/mariadb-es .
 			;;
 		* ) echo exiting...
 	esac
